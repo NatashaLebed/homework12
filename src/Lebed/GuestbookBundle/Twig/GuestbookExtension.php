fd514cceee79b291cda8f11188bf014e063cfcaf
NatashaLebed <natasha.lebed@gmail.com>
2014-02-25 13:17:56 +0200

    Del price twig-filter, fix template

--- src/Lebed/GuestbookBundle/Twig/GuestbookExtension.php
+++ src/Lebed/GuestbookBundle/Twig/GuestbookExtension.php
@@ -11,26 +11,17 @@ class GuestbookExtension extends \Twig_Extension
     public function getFilters()
     {
         return array(
-            new \Twig_SimpleFilter('price', array($this, 'priceFilter')),
             'ReadMore' => new \Twig_Filter_Method($this, 'readMoreFilter', array('is_safe' => array('html')) )
         );
     }
 
-    public function priceFilter($number, $decimals = 0, $decPoint = '.', $thousandsSep = ',')
-    {
-        $price = number_format($number, $decimals, $decPoint, $thousandsSep);
-        $price = '$'.$price;
-
-        return $price;
-    }
-
-    public function readMoreFilter($str, $link = '#', $limit = 20, $read_more_text = 'Read more')
+    public function readMoreFilter($str, $link = '#', $limit = 30, $read_more_text = 'Read more')
     {
         if (!is_null($str)){
             $array_str = explode(' ', $str, $limit + 1);
             array_pop($array_str); // delete last element of array
             $str = implode(' ', $array_str);
-            $str.= "...<p><a class='btn btn-info' href=\"".$link."\" role='button'>".$read_more_text."&raquo;</a></p>";
+            $str.= "...<p></p><p><a class='btn btn-info' href=\"".$link."\" role='button'>".$read_more_text."&raquo;</a></p>";
         }
 
         return $str;
