59ad62057dc15fa74e999eddfdea73bde6d151b9
NatashaLebed <natasha.lebed@gmail.com>
2014-03-13 08:12:47 +0200

    VK login

---
+++ src/Lebed/UserBundle/UserProvider/OAuthUserProvider.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Lebed\UserBundle\UserProvider;
+
+
+use HWI\Bundle\OAuthBundle\Security\Core\User\OAuthUserProvider as BaseOAuthUserProvider;
+
+class OAuthUserProvider extends BaseOAuthUserProvider
+{
+}
